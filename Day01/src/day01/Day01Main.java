c99c54eb469505f07d23d4b83b0ba0d7b4dfe19c
klaus <klaus@192.168.0.10>
2018-12-01 18:09:52 +0100

    Frequence calculation right
    First try to solve the second puzzle part

--- Day01/src/day01/Day01Main.java
+++ Day01/src/day01/Day01Main.java
@@ -1,10 +1,51 @@
 package day01;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
 public class Day01Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("testest");
+		BufferedReader br = null;
+		FileReader in = null;
+		int frequence = 0;
+		String nextLine = "";
+		ArrayList<Integer> pastFrequences = new ArrayList<Integer>();
+		
+		try {
+			in = new FileReader("./src/day01/Input.txt");
+			br = new BufferedReader(in);
+			
+			while((nextLine = br.readLine()) != null) {
+				pastFrequences.add(frequence);
+				frequence += Integer.parseInt(nextLine);
+				System.out.println(nextLine + "->" + frequence);
+				
+				if(pastFrequences.contains(frequence)) {
+					System.out.println("First time same frequence: " + frequence);
+				}
+				
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				br.close();
+				in.close();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
 	}
 
 }
