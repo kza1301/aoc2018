47e214c3a34176e64f99d09b164d3b8116057981
Klaus <klaus.zambach@ebcont.com>
2018-12-03 11:59:29 +0100

    First try of part1

--- Day03/src/day03/Day03.java
+++ Day03/src/day03/Day03.java
@@ -9,7 +9,6 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
@@ -22,12 +21,22 @@ public class Day03 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int rows = 1000;
+        int coloumns = 1000;
         BufferedReader br = null;
         FileReader in = null;
         String nextLine = "";
-        List<Square> squares = new ArrayList<>();
-        boolean[][] fabric = new boolean[1000][1000];
+        ArrayList<Square> squares = new ArrayList<>();
+        int[][] fabric = new int[coloumns][rows];
+        ArrayList<Integer> overlaps = new ArrayList<>();
+        int counter = 0;
 
+        for(int x = 0; x < coloumns-1; x++) {
+                for(int y = 0; y < rows-1; y++) {
+                    fabric[x][y] = 0;
+                }
+        }
+        
         try {
             in = new FileReader("./src/day03/Input03.txt");
             br = new BufferedReader(in);
@@ -46,6 +55,25 @@ public class Day03 {
                     e.getMessage();
             }	
         }
-    }
-    
+        
+        for(Square s : squares) {
+            for(int x = 0; x < s.getPosX()+s.getWidth(); x++) {
+                for(int y = 0; y < s.getPosY()+s.getHeight(); y++) {
+                    if(s.getPosX()+s.getWidth() < coloumns-1 && s.getPosY()+s.getHeight() < rows-1 && x < coloumns-1 && y < coloumns - 1) {
+                        if(fabric[x][y] != 0) {
+                            counter++;
+                            //overlaps.add(fabric[x][y]);
+                            //System.out.println("Added to overlaps:" + fabric[x][y]);
+                        }
+                        else {
+                            fabric[x][y] = s.getId();
+                        }
+                    }
+                }
+            }
+            System.out.println("Square " + s.getId() + " done");
+        }
+        System.out.println("Number of overlapping squares: " + counter);
+        //System.out.println("ID:" + squares.get(0).getId() + " POS:" + squares.get(0).getPosX() + "," + squares.get(0).getPosY() + "Dims:" + squares.get(0).getHeight() + "x" + squares.get(0).getWidth());
+    }    
 }
--- Day03/src/day03/Square.java
+++ Day03/src/day03/Square.java
@@ -10,18 +10,38 @@ package day03;
  * @author klaus.zambach
  */
 public class Square {
-    int id = 0;
-    int posX = 0;
-    int posY = 0;
-    int width = 0;
-    int height = 0;
+    private int id = 0;
+    private int posX = 0;
+    private int posY = 0;
+    private int width = 0;
+    private int height = 0;
     
     public Square(String s) {
         String[] parts = s.split(" ");
         id = Integer.parseInt(parts[0].substring(1));
-        posX = Integer.parseInt(parts[1].substring(0, parts[1].indexOf(",")-1));
-        posY = Integer.parseInt(parts[1].substring(parts[1].indexOf(",")+1, parts[1].length()-1));
-        height = Integer.parseInt(parts[2].substring(0, parts[2].indexOf("x")-1));
-        width = Integer.parseInt(parts[2].substring(parts[2].indexOf("x")+1));
+        posX = Integer.parseInt(parts[2].substring(0, parts[2].indexOf(",")));
+        posY = Integer.parseInt(parts[2].substring(parts[2].indexOf(",")+1, parts[2].length()-1));
+        height = Integer.parseInt(parts[3].substring(0, parts[3].indexOf("x")));
+        width = Integer.parseInt(parts[3].substring(parts[3].indexOf("x")+1));
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
     }
 }
